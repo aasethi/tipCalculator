c59c792395accaf27920161a90ec40d6ba351822
Ashma Sethi <ashmac@Ashmas-MacBook-Air-3.local>
2015-01-19 20:56:47 -0800

    Initial Commit

---
+++ tipcalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalc
+//
+//  Created by Ashma Sethi on 1/19/15.
+//  Copyright (c) 2015 Ashma Sethi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalc
+//
+//  Created by Ashma Sethi on 1/19/15.
+//  Copyright (c) 2015 Ashma Sethi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
